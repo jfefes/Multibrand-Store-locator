3a23c88fd265340f043c2f3142feb9aa1dbdee4d
James <jfefes@gmail.com>
2015-09-25 10:19:30 -0400

    checkbox on search results

--- app/views/search/results.blade.php
+++ app/views/search/results.blade.php
@@ -45,6 +45,15 @@
                     Dealer level:  <strong>{{ $dealer->category }}</strong> <br>
                   @endif
 
+                  <br>
+                  @if(isset($dealer->show_dealer))
+                  Dealer is
+                    @if($dealer->show_dealer==0)
+                    <strong>NOT</strong>
+                    @endif
+                  showing on locator
+                  @endif
+
                 </div>
               </div>
               <input type="submit" value="Edit" class="btn btn-success"/>
